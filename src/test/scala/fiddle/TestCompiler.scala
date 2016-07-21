d385a82af9a0658c6bfbc81278e5d6814cc434d7
Carl <carl@localhost>
2016-07-21 22:05:08 +0200

    Scala JS Compiler - Removing compiler dependency - Renaming group id

--- src/test/scala/fiddle/TestCompiler.scala
+++ src/test/scala/fiddle/TestCompiler.scala
@@ -10,7 +10,6 @@ package example
 
 import scala.scalajs.js
 import js.annotation._
-import org.scalajs.dom
 
 @JSExport
 class Foo(val x: Int) {
@@ -23,8 +22,6 @@ object HelloWorld extends js.JSApp {
   def main(): Unit = {
     println("Hello world!")
   }
-  
-  def alert = dom.window.alert("Hello!")
 }
     """
     
