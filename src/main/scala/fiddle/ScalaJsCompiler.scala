69844f51179fb22e3d62c9d6dc3159070c3eb368
Carl <carl@localhost>
2016-10-25 20:12:42 +0200

    Reading JAR file with source code and compiles. Added test

--- src/main/scala/fiddle/ScalaJsCompiler.scala
+++ src/main/scala/fiddle/ScalaJsCompiler.scala
@@ -4,11 +4,30 @@ import scala.language.postfixOps
 import scala.reflect.io.VirtualFile
 
 import org.slf4j.LoggerFactory
+import java.util.zip.ZipFile
+import java.util.zip.ZipEntry
+import scala.io.Source
+import java.util.zip.ZipInputStream
 
 class ScalaJsCompiler {
 
   val log = LoggerFactory.getLogger(getClass)
 
+    def compileJarWithScalaJsSource(context : ClassLoader, jarWithSource: ZipFile, optimizer: Optimizer, relativeJarPath: String): String = {
+    jarWithSource.entries match {
+      case entries => 
+        (new Iterator[ZipEntry] {
+          def next = entries.nextElement
+          def hasNext = entries.hasMoreElements
+        }).filter(entry => entry.getName.endsWith(".scala"))
+        .map(entry => Source.fromInputStream(jarWithSource.getInputStream(entry)).mkString)
+        .mkString match {
+          case source =>
+            compileScalaJsString(context, source, optimizer, relativeJarPath)
+        }
+    }
+  }
+  
   def compileScalaJsString(context : ClassLoader, source: String, optimizer: Optimizer, relativeJarPath: String): String = {
     compileScalaJsStrings(context, List(source), optimizer, relativeJarPath)
   }
---
+++ src/test/scala/fiddle/TestJarFile.scala
@@ -0,0 +1,24 @@
+package fiddle
+
+import org.junit.Test
+import java.util.zip.ZipInputStream
+import java.io.FileInputStream
+import java.util.zip.ZipFile
+import java.io.File
+
+class TestJarFile {
+  
+  @Test
+  def readJarFileWithScalaJsSource : Unit = {
+    new ScalaJsCompiler match {
+      case compiler =>
+        new ZipFile(getClass.getClassLoader.getResource("ScalaJsSource.jar").getFile) match {
+          case jarFile =>
+            compiler.compileJarWithScalaJsSource(getClass.getClassLoader, jarFile, Optimizer.Fast, "") match {
+              case compiled =>
+                println(s"Compiled size ${compiled.length}B")
+            }
+        }
+    }
+  }
+}
